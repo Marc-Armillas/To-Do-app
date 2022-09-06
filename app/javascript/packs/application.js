c96c39fbf7132ba4c127a24488d60d6aeb4d81a4
marc-armillas <marcarmillas@gmail.com>
2022-09-06 15:30:41 +0200

    fix bootstrap for heroku

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "stylesheets/application"
-import "bootstrap"
+import "bootstrap/dist/js/bootstrap"
+import "bootstrap/dist/css/bootstrap"
 import "channels"
 
 Rails.start()
